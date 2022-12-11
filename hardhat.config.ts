e4956745366ea99a89e05cb4b853f01a09c86ffa
l_silvestre98 <lsilvestre>
2022-12-11 01:02:33 +0000

    update hardhat config adn deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,20 +7,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const INFURA_KEY = process.env.INFURA_KEY!;
-const ALCHEMY_KEY = process.env.ALCHEMY_KEY!;
+const ALCHEMY_MAIN_KEY = process.env.ALCHEMY_MAIN_KEY!;
+const ALCHEMY_GOERLI_KEY = process.env.ALCHEMY_GOERLI_KEY;
 const METAMASK_DEV_PK = process.env.METAMASK_DEV_PK!;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   networks: {
     hardhat: {
+      allowUnlimitedContractSize: true,
+      chainId: 1,
       forking: {
-        url: `https://mainnet.infura.io/v3/${INFURA_KEY}`
+        // url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_MAIN_KEY}`,
+        blockNumber: 8111185 // set a block number from 10/12/22 so it doesnt have to fetch new data everytime
       }
     },
     goerli: {
+      chainId: 5,
       // url: `https://goerli.infura.io/v3/${INFURA_KEY}`
-      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_GOERLI_KEY}`,
       accounts: [ METAMASK_DEV_PK ]
     }
   }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,72 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import { GelatoOpsSDK, GELATO_ADDRESSES, isGelatoOpsSupported } from "@gelatonetwork/ops-sdk";
+import { TokenAddr } from "../interfaces";
+import { getUSDCTokenPairs } from "./queries";
+import { USDC_GOERLI_ADDRESS, USDC_MAINNET_ADDRESS, WBTC_GOERLI_ADDRESS, WETH_GOERLI_ADDRESS } from "../constants";
 
-async function main() {
-  /* const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const mainnetDeploy = async (chainId: number) => {
+  console.log('----- Fetching Tokens data -----\n');
+  const { usdcFirst, usdcSecond } = await getUSDCTokenPairs();
+  const uniqueSymbols = new Set<string>();
+  const allowedSwapTokens: TokenAddr[] = [ { addr: USDC_MAINNET_ADDRESS, symbol: 'USDC'} ];
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  [ ...usdcFirst, ...usdcSecond ].map((pool) =>{
+    let tokenToAdd: TokenAddr = undefined!;
+    if (pool.token0?.symbol.toLowerCase() === 'USDC'.toLowerCase()) {
+      tokenToAdd = { symbol: pool.token1?.symbol!, addr: pool.token1?.id! };
+    } else if (pool.token1?.symbol.toLowerCase() === 'USDC'.toLowerCase()) {
+      tokenToAdd = { symbol: pool.token0?.symbol!, addr: pool.token0?.id! };
+    }
+    if (!uniqueSymbols.has(tokenToAdd?.symbol)) {
+      allowedSwapTokens.push(tokenToAdd);
+      uniqueSymbols.add(tokenToAdd?.symbol);
+    } // else skip token
+  });
+  console.log('----- Tokens data Fetched-----\n');
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`); */
-  const DCA = await ethers.getContractFactory("SimpleDCA");
-  const dca = await DCA.deploy();
+  console.log('----- Deploying Contract -----\n');
+  const [ signer ] = await ethers.getSigners();
+  const gelatoOps = new GelatoOpsSDK(chainId, signer);
+  const dcaFactory = await ethers.getContractFactory("SimpleDCATask");
+  const dca = await dcaFactory.deploy(GELATO_ADDRESSES[chainId].ops, signer.address, allowedSwapTokens);
   
   await dca.deployed();
+  console.log('----- Contract Deployed -----\n');
+  console.log('Owner: ' + signer.address);
+  console.log('Contract: ' + dca.address);
+};
+
+const goerliDeploy = async (chainId: number) => {
+  const allowedSwapTokens: TokenAddr[] = [
+    { symbol: 'USDC', addr: USDC_GOERLI_ADDRESS },
+    { symbol: 'WETH', addr: WETH_GOERLI_ADDRESS },
+    { symbol: 'WBTC', addr: WBTC_GOERLI_ADDRESS }
+  ];
+  console.log('----- Deploying Contract -----\n');
+  const [ signer ] = await ethers.getSigners();
+  const gelatoOps = new GelatoOpsSDK(chainId, signer);
+  const dcaFactory = await ethers.getContractFactory("SimpleDCATask");
+  const dca = await dcaFactory.deploy(GELATO_ADDRESSES[chainId].ops, signer.address, allowedSwapTokens);
+  
+  await dca.deployed();
+  console.log('----- Contract Deployed -----\n');
+  console.log('Owner: ' + signer.address);
+  console.log('Contract: ' + dca.address);
+}
+
+const main = async () => {
+  const chainId = network.config.chainId as number;
+  if (!isGelatoOpsSupported(chainId)) throw new Error(`Gelato Ops network not supported (${chainId})`);
+
+  if (chainId === 1) {
+    console.log('-----Mainnet-----')
+    await mainnetDeploy(chainId);
+  } else if (chainId === 5) {
+    console.log('-----Goerli-----')
+    await goerliDeploy(chainId);
+  } else {
+    throw new Error('Contract is only supported in Goerli network');
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
